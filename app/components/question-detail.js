3121db18abb7b1b9e66cf1babd8c9f05481aea2b
sea413 <seanmp42@gmail.com>
2016-04-01 10:31:34 -0700

    transition to the comment section.

--- app/components/question-detail.js
+++ app/components/question-detail.js
@@ -1,10 +1,11 @@
-{{question.author}}, wishes to know. {{question.body}}.
+import Ember from 'ember';
 
-Here are their notes {{question.notes}}
-
-Please leave your answer below!
-<br>
-
-<p><img src={{question.image}}></p>
-
-<button {{action 'delete' question}}>Delete</button>
+export default Ember.Component.extend({
+  actions: {
+    delete(question) {
+      if (confirm('Are you sure you want to delete this question?')) {
+        this.sendAction('destroyQuestion', question);
+      }
+    }
+  }
+});
