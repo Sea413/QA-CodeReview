5936adef58f9f86cc4f52205102f02dbf7712ab9
sea413 <seanmp42@gmail.com>
2016-04-01 10:23:13 -0700

    Add the details for each question submitted.

---
+++ app/components/question-detail.js
@@ -0,0 +1,10 @@
+{{question.author}}, wishes to know. {{question.body}}.
+
+Here are their notes {{question.notes}}
+
+Please leave your answer below!
+<br>
+
+<p><img src={{question.image}}></p>
+
+<button {{action 'delete' question}}>Delete</button>
